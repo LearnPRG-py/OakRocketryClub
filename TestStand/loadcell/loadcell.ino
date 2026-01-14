8f414c8c39b6bef63cdeac05156adfeb01f60def
LearnPRG-py <aryankrishnan4b@gmail.com>
2026-01-14 23:15:42 +0530

    Update loadcell to use storage system
    tried using std::vector for the first time on arduino lmk if this compiles.

--- TestStand/loadcell/loadcell.ino
+++ TestStand/loadcell/loadcell.ino
@@ -1,14 +1,23 @@
 #include "HX711.h"
+#include <vector>
 
 #define DOUT 3
 #define SCK  2
 
+// So that the array can grow on it's own
+std::vector<float> buf;
+
 HX711 scale;
 
 float calibrationFactor = 1.0;
+bool fillToBuffer = false;
+bool filledToBuffer = false;
+constexpr int endCheckFrames = 10; // Will check 330ms of low reading to detect end.
+int motorZeroFrames = 0;
 
 void setup() {
   Serial.begin(9600);
+  buf.reserve(200);
   scale.begin(DOUT, SCK);
 
   Serial.println("Remove all weight");
@@ -23,12 +32,33 @@ void setup() {
 
   calibrationFactor = reading / knownWeight;
   scale.set_scale(calibrationFactor);
+  //TODO: Find this in the test room and then keep these as constexprs
 
   Serial.println("Calibration complete");
 }
 
 void loop() {
   Serial.print("Weight (kg): ");
-  Serial.println(scale.get_units(10));
-  delay(500);
+  float reading = scale.get_units(10);
+  if (reading > 0.05) {
+    fillToBuffer = true;
+  }
+  if (fillToBuffer) {
+    buf.push_back(reading);
+    if (reading < 0.01){
+      ++motorZeroFrames;
+      if (motorZeroFrames > endCheckFrames){
+        fillToBuffer = false;
+        filledToBuffer = true;
+      }
+    }
+    else {
+      motorZeroFrames = 0;
+    }
+  }
+  if (filledToBuffer) {
+    for (float v : buf) {
+      Serial.println(v, 3);
+    }
+  }
 }
